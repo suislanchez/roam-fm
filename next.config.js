b7a3bc9415e2043b46b25b5e3e0888cd284504da
suislanchez <luisanchez@berkeley.edu>
2025-06-06 16:07:04 -0700

    9

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
@@ -11,6 +12,9 @@ const nextConfig = {
         hostname: '**',
       }
     ]
+  },
+  experimental: {
+    appDir: true
   }
 };
 
